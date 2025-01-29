05f89b6514e4dde60d8ace91d887c93259d28d0a
futenma <futenma@trymore-inc.jp>
2025-01-29 12:42:58 +0900

    upgrade: パッケージのバージョンアップ

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 import js from '@eslint/js'
-import astro from 'eslint-plugin-astro'
 
 export default [
   js.configs.recommended,
-  ...astro.configs['flat/recommended'],
   {
     files: ['src/**/*.js'],
     languageOptions: {
